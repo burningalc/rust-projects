2b576bb88ec9ede35300199575efa45eddcd65d8
kennysliding <kennychiu.v@gmail.com>
2023-04-06 04:03:56 +0800

    solution with O(n)

--- jump-game/src/lib.rs
+++ jump-game/src/lib.rs
@@ -1,29 +1,17 @@
 pub struct Solution;
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        // create a memo table to record if an index can be reached
-        let mut memo = vec![false; nums.len()];
-        memo[0] = true;
-        for (index, &num) in nums.iter().enumerate() {
-            if index == nums.len() {
-                break;
-            }
-
-            // only jump from this position if we can jump to this position
-            if !memo[index] {
-                continue;
-            }
-
-            // mark all possible jumping position to be true
-            for i in 1..num + 1 {
-                let jumping_index = index + i as usize;
-                if jumping_index >= nums.len() {
-                    break;
-                }
-                memo[jumping_index] = true;
-            }
+        if nums.len() == 1 {
+            return true;
         }
 
-        *memo.last().unwrap()
+        let mut memo = vec![0; nums.len()];
+        memo[0] = nums[0];
+
+        for (index, &num) in nums.iter().enumerate().skip(1) {
+            memo[index] = std::cmp::max(num, memo[index - 1] - 1);
+        }
+
+        !memo.iter().take(memo.len() - 1).any(|e| *e == 0)
     }
 }
--- jump-game/tests/test.rs
+++ jump-game/tests/test.rs
@@ -7,3 +7,27 @@ fn test_1() {
 
     assert_eq!(Solution::can_jump(input), expected_output)
 }
+
+#[test]
+fn test_2() {
+    let input = vec![3, 2, 1, 0, 4];
+    let expected_output = false;
+
+    assert_eq!(Solution::can_jump(input), expected_output)
+}
+
+#[test]
+fn test_3() {
+    let input = vec![0];
+    let expected_output = true;
+
+    assert_eq!(Solution::can_jump(input), expected_output)
+}
+
+#[test]
+fn test_4() {
+    let input = vec![2, 0, 0];
+    let expected_output = true;
+
+    assert_eq!(Solution::can_jump(input), expected_output)
+}
