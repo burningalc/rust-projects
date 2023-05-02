eb35561b820a7e64bdd3caf0fdbf5f9b9a8f5af1
kennysliding <kennychiu.v@gmail.com>
2023-05-02 21:16:13 +0800

    test update

--- sign-of-the-product-of-an-array/tests/test.rs
+++ sign-of-the-product-of-an-array/tests/test.rs
@@ -22,7 +22,7 @@ fn test_2() {
 fn test_3() {
     let nums: Vec<i32> = vec![-1, -2, -3, -4, 3, 2, 1];
 
-    let expected_output = 2;
+    let expected_output = 1;
 
     assert_eq!(Solution::array_sign(nums), expected_output);
 }
