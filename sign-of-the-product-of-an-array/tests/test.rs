8fe014527ea5d508789b93535c64e634025b941a
kennysliding <kennychiu.v@gmail.com>
2023-05-02 21:15:44 +0800

    leetcode q1822

---
+++ sign-of-the-product-of-an-array/tests/test.rs
@@ -0,0 +1,28 @@
+use sign_of_the_product_of_an_array::Solution;
+
+#[test]
+fn test_1() {
+    let nums: Vec<i32> = vec![-1, -2, -3, -4, 3, 2, 1];
+
+    let expected_output = 1;
+
+    assert_eq!(Solution::array_sign(nums), expected_output);
+}
+
+#[test]
+fn test_2() {
+    let nums: Vec<i32> = vec![-1, 1, -1, 1, -1];
+
+    let expected_output = -1;
+
+    assert_eq!(Solution::array_sign(nums), expected_output);
+}
+
+#[test]
+fn test_3() {
+    let nums: Vec<i32> = vec![-1, -2, -3, -4, 3, 2, 1];
+
+    let expected_output = 2;
+
+    assert_eq!(Solution::array_sign(nums), expected_output);
+}
